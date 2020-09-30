a3417ba438fc21a00fe77b468ee1bf73a4588609
ReyVega <A01114523@itesm.mx>
2020-09-30 03:45:19 +0000

    Practica5

--- practicas/5/mywc.c
+++ practicas/5/mywc.c
@@ -4,31 +4,27 @@
 
 int main() {
     char c;
-    unsigned caracteres = -1;
+    unsigned caracteres = 0;
     unsigned palabras = 0;
     unsigned lineas = 0;
     unsigned bandera = 0;
-    unsigned bandera2 = 0;
 
     while(read(STDIN_FILENO,&c,1) != 0) {
-        if(c == ' ')  {
+		if(c == '\n') {
+            caracteres++;
+            palabras++;
+            lineas++;
+            bandera = 0;
+        } else if(c != ' ') {
+            caracteres++;
+            bandera = 0;
+        } else {
             if(bandera == 0) {
                 caracteres++;
                 palabras++;
                 bandera = 1;
             } else {
                 caracteres++;
-            }
-        } else if(c == '\n'){
-            caracteres++;
-            lineas++;
-            bandera = 0;
-        } else {
-            caracteres++;
-            bandera = 0;
-            if(bandera2 == 0) {
-                palabras++;
-                bandera2 = 1;
             }
         }
     }
